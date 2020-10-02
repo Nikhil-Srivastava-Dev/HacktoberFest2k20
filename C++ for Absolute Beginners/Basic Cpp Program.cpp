c705507c8633d8c40f2a96137182c184fc1d0db1
Nikhil Srivastava <68149981+Nikhil-Srivastava-Dev@users.noreply.github.com>
2020-10-02 20:21:58 +0530

    Basic Cpp Program.cpp

---
+++ C++ for Absolute Beginners/Basic Cpp Program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>    // We are including the 'iostream' header file in this cpp source file
+using namespace std;   // using the std abbreviation to make it easier for us for writing future code and not using the keyword 'std' in every function
+int main(){            // we are starting the main function here, the main function is the main area where where all functions are run
+  
+  cout<<"Hello World"<<endl; // this is the 'cout' function, this is used to print string typed data in the command line and the keyword 'endl' helps us to give a single line space after priting the data in the command line
+
+  return 0;  // we are returning 0 at the end so if all thing before this are correct, the program runs smoothly!
+}
+
+// The // (double slash) is used to give single line comments in C++
+
+
+/*
+A pair of /* and */ is used to give multiline comments in C++
+*/
